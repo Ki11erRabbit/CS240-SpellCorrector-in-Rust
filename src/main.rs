f7b39ed6956d68c0eb63d9ac84f6f17dde3b0304
ki11errabbit <ki11errabbit@posteo.net>
2022-10-02 21:25:08 -0600

    Trie has been implemented

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod Trie;
+
+fn main() {
+    println!("Hello, world!");
+
+
+    let mut dictionary = Trie::Trie::new();
+
+    dictionary.add("aba".to_string());
+    dictionary.add("aca".to_string());
+
+    println!("{}",dictionary.to_string());
+}
